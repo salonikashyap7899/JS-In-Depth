97d218ba03e338c9fd5e5c04dfeefbd0222abe60
Saloni Kashyap <124189127+salonikashyap7899@users.noreply.github.com>
2026-04-08 09:51:25 +0530

    Add Readme

--- promises/asyn/await/script.js
+++ promises/asyn/await/script.js
@@ -1,13 +1,17 @@
+// The async function declaration creates a binding of a new async function to a given name. T
+// he await keyword is permitted within the function body, enabling asynchronous, 
+// promise-based behavior to be written in a cleaner style and avoiding the need to
+//  explicitly configure promise chains.
 let a = 10
 let b = 20 
 
 let result = a + b
 console.log(result)
 
-// async function getData (){
-//    let resultFromServer = await fetch("https://jsonplaceholder.typicode.com/posts")
-//    console.log(await resultFromServer.json())
-// }
+async function getData (){
+   let resultFromServer = await fetch("https://jsonplaceholder.typicode.com/posts")
+   console.log(await resultFromServer.json())
+}
 
 fetch("https://jsonplaceholder.typicode.com/posts")
 .then((data) => {
